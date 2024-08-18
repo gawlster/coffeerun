e2397b3989c67686d0599b3ca93cf478dc148072
Connor Gawley (Desktop) <connorgawley@uvic.ca>
2024-08-18 14:30:17 -0700

    Show score on game over screen

--- Assets/Scripts/GameOverScreenController.cs
+++ Assets/Scripts/GameOverScreenController.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverScreenController : MonoBehaviour {
+    private GameObject score;
+    private TextMeshProUGUI scoreText;
+    
     private void Start() {
+        score = GameObject.Find("GameOverScoreText");
+        scoreText = score.GetComponent<TextMeshProUGUI>();
+        
         GameStateManager.Instance.SubscribeToGameStateChanged(OnGameStateChanged);
         gameObject.SetActive(false);
     }
 
     private void OnGameStateChanged(GameStateManager.GameStates gameState) {
+        scoreText.SetText($"Score: {InventoryManager.Instance.CoinCount}");
         gameObject.SetActive(gameState == GameStateManager.GameStates.GameOver);
     }
 
